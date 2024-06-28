31c20ae6f308d6f66e5d78fd5ebc894b22a52dde
Nils Bandener <nils.bandener@eliatra.com>
2024-06-28 18:00:54 +0200

    Code cleanup
    Signed-off-by: Nils Bandener <nils.bandener@eliatra.com>

--- src/main/java/org/opensearch/security/privileges/PrivilegesEvaluator.java
+++ src/main/java/org/opensearch/security/privileges/PrivilegesEvaluator.java
@@ -327,8 +327,6 @@ public class PrivilegesEvaluator {
 
         PrivilegesEvaluatorResponse presponse = new PrivilegesEvaluatorResponse();
 
-        final SecurityRoles securityRoles = getSecurityRoles(mappedRoles);
-
         // Add the security roles for this user so that they can be used for DLS parameter substitution.
         user.addSecurityRoles(mappedRoles);
         setUserInfoInThreadContext(user);
@@ -485,7 +483,6 @@ public class PrivilegesEvaluator {
         }
 
         ImmutableSet<String> allIndexPermsRequired = evaluateAdditionalIndexPermissions(request, action0);
-        // final String[] allIndexPermsRequiredA = allIndexPermsRequired.toArray(new String[0]);
 
         if (isDebugEnabled) {
             log.debug(
