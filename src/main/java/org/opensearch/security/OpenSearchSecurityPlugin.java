b5c1d304459a85821ee0209d48df8ba04cace523
Nils Bandener <nils.bandener@eliatra.com>
2024-07-02 08:28:30 +0200

    Narrowed use of ClusterService to ClusterService::state
    Signed-off-by: Nils Bandener <nils.bandener@eliatra.com>

--- src/main/java/org/opensearch/security/OpenSearchSecurityPlugin.java
+++ src/main/java/org/opensearch/security/OpenSearchSecurityPlugin.java
@@ -1067,7 +1067,7 @@ public final class OpenSearchSecurityPlugin extends OpenSearchSecuritySSLPlugin
         this.salt = Salt.from(settings);
 
         final IndexNameExpressionResolver resolver = new IndexNameExpressionResolver(threadPool.getThreadContext());
-        irr = new IndexResolverReplacer(resolver, clusterService, cih);
+        irr = new IndexResolverReplacer(resolver, clusterService::state, cih);
 
         final String DEFAULT_INTERCLUSTER_REQUEST_EVALUATOR_CLASS = DefaultInterClusterRequestEvaluator.class.getName();
         InterClusterRequestEvaluator interClusterRequestEvaluator = new DefaultInterClusterRequestEvaluator(settings);
@@ -1119,6 +1119,7 @@ public final class OpenSearchSecurityPlugin extends OpenSearchSecuritySSLPlugin
 
         evaluator = new PrivilegesEvaluator(
             clusterService,
+            clusterService::state,
             threadPool.getThreadContext(),
             cr,
             resolver,
